a870a4c73c73792f61186eb235c46214bbf767db
sts19813 <sts19813@gmail.com>
2026-08-10 15:30:19 -0600

    cambios para asignacion de tecnico responsable a la propiedad

---
+++ app/Mail/PropertyTechnicianAssignedMail.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\MaintenanceProvider;
+use App\Models\Property;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Support\Collection;
+use Illuminate\Queue\SerializesModels;
+
+class PropertyTechnicianAssignedMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public function __construct(
+        public Property $property,
+        public MaintenanceProvider $technician,
+        public Collection $tickets,
+    ) {
+    }
+
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Propiedad asignada con tickets pendientes: '.$this->property->internal_name,
+        );
+    }
+
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.property-technician-assigned',
+            with: [
+                'property' => $this->property,
+                'technician' => $this->technician,
+                'tickets' => $this->tickets,
+                'maintenanceUrl' => route('maintenance.index', ['property' => $this->property->uuid]),
+                'loginUrl' => url('/login'),
+                'logoUrl' => asset('assets/img/suhomes-app-logo.png'),
+                'appName' => 'SuHomes',
+            ],
+        );
+    }
+}
